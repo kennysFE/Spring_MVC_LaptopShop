8232e9d04c7c0a06fbdf8ecd6c3782fed22eb448
hoangFE <metmoi556@email.com>
2024-10-01 00:15:12 +0700

    Hello world

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String index() {
-		return "Hello World from Spring Boot 2024 ";
+		return " Hello World ";
 	}
 
 }
