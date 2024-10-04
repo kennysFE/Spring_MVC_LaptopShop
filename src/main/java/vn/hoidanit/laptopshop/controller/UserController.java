3fd5cdb163634f877049f2de8ab408e74d6529e5
hoangFE <metmoi556@email.com>
2024-10-04 15:46:14 +0700

    Feature Get All User

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -33,14 +33,20 @@ public class UserController {
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users1", users);
+        return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/create") // method get
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST) // method post
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println(" run here" + hoidanit);
         this.userService.handleSaveUser(hoidanit);
-        return "hello";
+        return "redirect:/admin/user";
     }
 }
--- src/main/webapp/WEB-INF/view/admin/user/create.jsp
+++ src/main/webapp/WEB-INF/view/admin/user/create.jsp
@@ -7,7 +7,7 @@ uri="http://www.springframework.org/tags/form" prefix="form"%>
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Create user</title>
     <!-- Latest compiled and minified CSS -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
@@ -27,7 +27,7 @@ uri="http://www.springframework.org/tags/form" prefix="form"%>
           <hr />
           <form:form
             method="post"
-            action="/admin/user/create1"
+            action="/admin/user/create"
             modelAttribute="newUser"
           >
             <div class="mb-3">
