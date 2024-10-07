59ffcbba30a79f21e2f9bf010e14d68bf5373893
hoangFE <metmoi556@email.com>
2024-10-07 19:29:20 +0700

    Many to Many RelationShip

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,11 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,10 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    // one order -> to -> many order details
+    @OneToMany(mappedBy = "order")
+    List<OrderDetail> orderDetails;
 
     // Getters and setters
     public long getId() {
