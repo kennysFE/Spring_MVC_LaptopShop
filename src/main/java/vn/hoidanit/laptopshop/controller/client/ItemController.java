4b9693ab38941c52a186606b6c7e05484f8db60f
hoangFE <metmoi556@email.com>
2024-10-18 18:30:30 +0700

    Biding data homepage

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,15 +1,29 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductPage(Model model, @PathVariable long id) {
+        Product productItem = this.productService.getProductById(id).get();
+        model.addAttribute("id", id);
+        model.addAttribute("productItem", productItem);
         return "client/product/detail";
     }
 
