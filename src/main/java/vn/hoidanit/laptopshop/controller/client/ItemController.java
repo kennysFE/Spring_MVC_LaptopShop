ff5f06a9104e2c8f22bfb4464566e0c3c44d5914
hoangFE <metmoi556@email.com>
2024-11-02 22:37:13 +0700

    Design filter product page

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -2,7 +2,11 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -183,6 +187,29 @@ public class ItemController {
         this.productService.handleAddProductToCart(email, id, session, quantity);
 
         return "redirect:/product/" + id;
+    }
+
+    @GetMapping("/products")
+    public String getProductsPage(Model model, @RequestParam("page") Optional<String> pageOptional) {
+        int page = 1;
+
+        try {
+            if (pageOptional.isPresent()) {
+                page = Integer.parseInt(pageOptional.get());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, 6);
+        Page<Product> pageProduct = this.productService.getProductPagination(pageable);
+        List<Product> products = pageProduct.getContent();
+
+        model.addAttribute("products", products);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", pageProduct.getTotalPages());
+
+        return "client/product/show";
     }
 
 }
